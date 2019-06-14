a4c38865b44008f45db5175dc75142006a584b32
Lukas Elmer <lukas.elmer@gmail.com>
2019-06-14 10:39:33 +0200

    add code example

---
+++ src/infer-enum.ts
@@ -0,0 +1,18 @@
+export const iAmAModule = true;
+
+const colors = ['red', 'blue', 'green'] as const;
+
+type Color = typeof colors extends readonly (infer R)[] ? R : never;
+const color: Color = 'green';
+console.log(color);
+
+type EnumFromArray<T> = T extends readonly (infer R)[] ? R : never;
+type Color2 = EnumFromArray<typeof colors>;
+const name = 'bla';
+const color2: Color2 = 'blue';
+console.log(color2);
+
+type Color3 = EnumFromArray<typeof name>;
+let color3: Color3;
+// color3 = null; Error: Type 'null' is not assignable to type 'never'
+console.log(color3);
